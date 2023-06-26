77882052ab43628b6e7304e5913d121ad2b00f7c
NinoTheMeano <tramp2006ayahoo.com@gmail.com>
2023-06-26 13:30:00 -0400

    1.0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup_info = {
     "name": "incantoos",
-    "version": "1.0.1",
+    "version": "1.0.1.2",
     "author": "NinoTheMeano",
     "author_email": "ninosdeveloping@gmail.com",
     "description": "IncantoOS Is intended to give ",
